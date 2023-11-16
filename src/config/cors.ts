7ef57a95d48bb74abced56d0eb6c302157fb46c9
ebonian <tanadon.santisan@gmail.com>
2023-11-16 19:14:21 +0700

    chore: allow chulaintaniarun2024.com cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,6 +5,8 @@ const corsOptions: CorsOptions = {
         'http://localhost:3000',
         'https://run.chula.engineering',
         'https://staging.run.chula.engineering',
+        'https://chulaintaniarun2024.com',
+        'https://preview.chulaintaniarun2024.com',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
