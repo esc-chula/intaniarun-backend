2e2a34fcb9af405722738bb17efbf6881c5fb420
LittleNa1000 <naphanccnp@gmail.com>
2023-11-17 19:38:58 +0700

    fix: enable checkAuthToken

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -4,14 +4,14 @@ import { envOrFail } from '@/utils/env';
 
 const checkAuthToken = (req: Request, res: Response, next: NextFunction) => {
     try {
-        return next();
+        // return next();
         const token = req.headers['x-auth-token'];
         if (!token) throw new Error('Token not found');
         if (token !== envOrFail('X_AUTH_TOKEN'))
             throw new Error('Token mismatch');
         return next();
     } catch (error) {
-        return next();
+        // return next();
         // console.log(error);
         res.status(403).json({ error: 'Unauthorized.' });
     }
