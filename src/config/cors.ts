3367b4150c51a3b66c5ca69e3a43efbc652d6ec4
ebonian <tanadon.santisan@gmail.com>
2023-11-17 23:30:31 +0700

    fix

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,7 @@ const corsOptions: CorsOptions = {
         'http://localhost:3000',
         'https://run.chula.engineering',
         'https://staging.run.chula.engineering',
-        'https://chulaintaniarun2024.com',
+        'https://www.chulaintaniarun2024.com',
         'https://preview.chulaintaniarun2024.com',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
