027f4b57d34ae04eefb64fcb8f39852d63e84747
ebonian <tanadon.santisan@gmail.com>
2023-12-15 18:09:09 +0700

    chore: disable cron

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import cron from 'node-cron';
 
 import server from '@/app/server';
 
-import { sendEmail } from './cron/sendEmail';
+// import { sendEmail } from './cron/sendEmail';
 
 process.env.TZ = 'Asia/Bangkok';
 
 server.serve();
 
 cron.schedule('*/30 * * * *', () => {
-    sendEmail();
+    // sendEmail();
 });
 
 // cron.schedule('36 8 */1 * *', () => {
